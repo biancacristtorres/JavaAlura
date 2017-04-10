575086f9073ce858208d50d10c1089ed691efcf9
biancacristtorres <biancacristtorres34@gmail.com>
2017-04-10 11:04:16 -0300

    Atividades Primeira Aula

---
+++ JavaI PrimeirosPassos/MeuProgramaModificado.java
@@ -0,0 +1,6 @@
+class MeuProgramaModificado {
+	public static void main(String[] args) {
+		System.out.println("Esta é a minha classe modificada Java!\n Segunda Linha!");
+		System.out.println("Choraa!");
+	}
+}
