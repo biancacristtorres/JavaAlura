9e6443cd54b56e3b60fd43251410e84bec01e215
biancacristtorres <biancacristtorres34@gmail.com>
2017-04-10 17:21:47 -0300

    Programa.java e Programa.class

---
+++ JavaI PrimeirosPassos/Programa.java
@@ -0,0 +1,48 @@
+class Programa {
+	public static void main(String[] args) {
+		int idadeJoao = 18;
+		int idadeMaria = 21;
+		
+		int somaIdades = idadeJoao + idadeMaria;
+		System.out.println(somaIdades);
+		
+		double pi = 3.14;
+		double doubropi = pi * 2;
+		
+		System.out.println(doubropi);
+		
+		boolean amigo = true;
+		boolean inimigo = !amigo;
+		
+		boolean maiorDeIdade = idadeJoao <= 18;
+		
+		char letra = 'M';
+		String nomeCompleto = "João da Silva";
+		
+		System.out.println(nomeCompleto);
+		
+		//////////////////////////////////
+		
+		int numeroIntermediario = 3200;
+		long numeroGrande = 99999999999L;
+		
+		float outroPi = 3.14;
+		double pi = 3.14;
+		
+		long numero = 314;
+		int copiaDeNumero = (int) numero;
+		
+		float copia
+		
+		/////////////////////////////
+		
+		
+		int idadeVisitante = 15;
+		
+		if(idadeVisitante>=18){
+			System.out.println("Entra na Festa!!");
+		} else {
+			System.out.println("Entra na Festa!!");
+		}	
+	}
+}
